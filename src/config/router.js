b84535e246baa821d302c4817e6e51995831d246
GlaucioDias <fleevrl@gmail.com>
2021-08-03 17:55:29 -0300

    seção 8-55: deve listar apenas as transações do usuário

--- src/config/router.js
+++ src/config/router.js
@@ -4,6 +4,7 @@ module.exports = (app) => {
     app.use('/auth', app.routes.auth);
     protectedRouter.use('/users', app.routes.users);
     protectedRouter.use('/accounts', app.routes.accounts);
+    protectedRouter.use('/transactions', app.routes.transactions);
 
     app.use('/v1', app.config.passport.authenticate(), protectedRouter);
 }
