381200893896b2f316f7012a342caadc8d969629
Glaucio Dias <fleevrl@gmail.com>
2021-08-02 22:42:57 -0300

    seção 7-51: Não deve inserir conta com nome duplicado

--- test/routes/account.test.js
+++ test/routes/account.test.js
@@ -4,54 +4,71 @@ const jwt = require('jwt-simple');
 const app = require('../../src/app');
 const MAIN_ROUTE = '/v1/accounts';
 let user;
+let user2;
 
-beforeAll(async () => {
+beforeEach(async () => {
     const result = await app.services.user.save({ name: 'User Account', mail: `${Date.now()}@test.com`, passwd: '123456' })
 
     user = { ...result[0] };
-    user.token = jwt.encode(user, 'Segredo!')
+    user.token = jwt.encode(user, 'Segredo!');
+
+    const result2 = await app.services.user.save({ name: 'User Account2', mail: `${Date.now()}@test.com`, passwd: '123456' })
+
+    user2 = { ...result2[0] };
 });
 
 test('Deve inserir uma conta com sucesso', () => {
     return request(app)
         .post(MAIN_ROUTE)
-        .send({ name: '#Acc 1', user_id: user.id })
+        .send({ name: '#Acc 1' })
         .set('authorization', `bearer ${user.token}`)
         .then((result) => {
             expect(result.status).toBe(201);
             expect(result.body.name).toBe('#Acc 1');
             expect(result.body).not.toHaveProperty('passwd')
-        })
+        });
 });
 
 test('Não deve inserir uma conta sem nome', () => {
     return request(app)
         .post(MAIN_ROUTE)
-        .send({ user_id: user.id })
+        .send({})
         .set('authorization', `bearer ${user.token}`)
         .then((res) => {
             expect(res.status).toBe(400);
             expect(res.body.error).toBe('Nome é um atributo obrigatório');
-        })
+        });
 });
 
-test.skip('Não deve inserir uma conta de nome duplicado para o mesmo usuário', () => { });
-
-test('Deve listar todas as contas', () => {
+test('Não deve inserir uma conta de nome duplicado para o mesmo usuário', () => {
     return app.db('accounts')
-        .insert({ name: 'Acc list', user_id: user.id })
+    .insert({ name: 'Acc duplicada', user_id: user.id })
+    .then(() => request(app).post(MAIN_ROUTE)
+    .send({ name: 'Acc duplicada'})
+    .set('authorization', `bearer ${user.token}`)
+    )
+    .then((res) => {
+        expect(res.status).toBe(400);
+        expect(res.body.error).toBe('Já existe uma conta com este nome')
+    })
+ });
+
+test('Deve listar apenas as contas do usuário', () => {
+    return app.db('accounts')
+        .insert([
+            { name: 'Acc user #1', user_id: user.id },
+            {name: 'Acc user #2', user_id: user2.id },
+        ])
         .then(() => request(app)
             .get(MAIN_ROUTE)
             .set('authorization', `bearer ${user.token}`)
         )
         .then((res) => {
             expect(res.status).toBe(200);
-            expect(res.body.length).toBeGreaterThan(0);
-        })
-
+            expect(res.body).toHaveLength(1);
+            expect(res.body[0].name).toBe('Acc user #1');
+        });
 });
-
-test.skip('Deve listar apenas as contas do usuário', () => { });
 
 
 test('Deve retornar uma conta por Id', () => {
@@ -64,7 +81,7 @@ test('Deve retornar uma conta por Id', () => {
             expect(res.status).toBe(200);
             expect(res.body.name).toBe('Acc By Id');
             expect(res.body.user_id).toBe(user.id);
-        })
+        });
 });
 
 test.skip('Não deve retornar uma conta de outro usuário', () => { });
@@ -82,7 +99,7 @@ test('Deve alterar uma conta', () => {
         .then((res) => {
             expect(res.status).toBe(200);
             expect(res.body.name).toBe('Acc Updated');
-        })
+        });
 });
 
 test.skip('Não deve alterar uma conta de outro usuário', () => { });
@@ -95,7 +112,7 @@ test('Deve remover uma conta', () => {
             .set('authorization', `bearer ${user.token}`))
         .then((res) => {
             expect(res.status).toBe(204);
-        })
+        });
 });
 
 test.skip('Não deve remover uma conta de outro usuário', () => { });
